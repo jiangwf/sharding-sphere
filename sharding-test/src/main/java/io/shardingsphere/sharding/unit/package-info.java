d1835a64772c5709d778b50e2f12d8d5d5991532
姜伟锋 <jiangweifeng@jiangweifengdeMacBook-Pro.local>
2018-06-26 11:01:47 +0800

    搭建测试moudle

--- sharding-test/src/main/java/io/shardingsphere/sharding/unit/package-info.java
+++ sharding-test/src/main/java/io/shardingsphere/sharding/unit/package-info.java
@@ -1,4 +1,4 @@
 /**
  * weifeng.jiang 2018-06-26 10:54
  */
-package io.shardingsphere.sharding.test;
+package io.shardingsphere.sharding.unit;
--- sharding-test/src/test/java/io/shardingsphere/sharding/test/dao/package-info.java
+++ sharding-test/src/test/java/io/shardingsphere/sharding/test/dao/package-info.java
@@ -1,4 +1,4 @@
 /**
- * weifeng.jiang 2018-06-26 10:56
+ * weifeng.jiang 2018-06-26 10:57
  */
 package io.shardingsphere.sharding.test.dao;
